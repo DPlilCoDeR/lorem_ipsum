8581137eaf7765e8c16aabd0353e2b740ba7dba6
Jason Nicolas Carvajal Perez <doble_papi@MacBook-Pro.local>
2022-02-16 10:45:29 +0100

    create form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React, { useState } from 'react';
 import data from './data';
+
+
 function App() {
+  const [paragraphs, setParagraphs] = useState(0)
   
-  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`you want ${paragraphs}`)
+  }
+
+
   return (
-  <h2>lorem ipsum project setup</h2>
+  <main className='section'>
+    <h3 className='title'>Cool LoremIpsum</h3>
+    <section className='section-center'>
+      <form onSubmit={handleSubmit}>
+        <label>Paragraphs: </label>
+        <input type='number' value={paragraphs} min='1' max='8' onChange={(e) => setParagraphs(e.target.value)}></input>
+        <button className='btn' type='submit'>Generate</button>
+      </form>
+    </section>
+    <section className='result'></section>
+  </main>
     )
 }
 
