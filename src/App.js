63effd2ec079597b779b9032051d7784c81e0c30
Jason Nicolas Carvajal Perez <doble_papi@MacBook-Pro.local>
2022-02-17 12:06:05 +0100

    last details, Finish!!!

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
     e.preventDefault();
     let amount = parseInt(quantity)
     setParagraphs(data.slice(0, amount));
+    setQuantity(1);
   };
 
 
@@ -17,19 +18,17 @@ function App() {
   <main className='section'>
     <h3 className='title'>Cool Lorem Ipsum</h3>
     <section className='section-center'>
-      <form onSubmit={handleSubmit}>
+      <form className='lorem-form' onSubmit={handleSubmit}>
         <label>Paragraphs: </label>
         <input type='number' value={quantity} min='1' max='8' onChange={(e) => setQuantity(e.target.value)}></input>
         <button className='btn' type='submit'>Generate</button>
       </form>
     </section>
-    <section className='result'>
       <article className='lorem-text'>
         {paragraphs.map((text, index)=> {
           return <p key={index}>{text}</p>
         })}
       </article>
-    </section>
   </main>
     )
 }
